0c3371d2f8a063ff90517ac502a493176ff28eab
Pierre-Yves Rollo <dev@pyrollo.com>
2019-04-17 16:24:38 +0200

    Cosmetic and licensing

--- init.lua
+++ init.lua
@@ -1,3 +1,21 @@
+--[[
+	ffiopt mod for Minetest - A mod for optimizing memory and speed of
+	VoxelManip and PerlinNoiseMap
+	(c) EvidenceBKidscode
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Lesser General Public License as published
+	by the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--]]
 
 ffiopt = {}
 ffiopt.name = minetest.get_current_modname()
